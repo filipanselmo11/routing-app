f2adc1412b7779ce55aa4601a44739b40b0ea85a
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-01-27 10:45:57 -0400

    feat: APP ROUting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'routing-app';
+  // @Input()
+  // set id(heroId: string) {
+  //   this.hero$ = this.service.getHero
+  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
+import { SecondComponent } from './components/second/second.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ChildAComponent } from './components/child-a/child-a.component';
+import { ChildBComponent } from './components/child-b/child-b.component';
 
-export const routes: Routes = [];
+const resolveChildATitle: ResolveFn<string> = () => Promise.resolve('child a');
+
+
+export const routes: Routes = [
+    {
+        path: 'first-component',
+        title: 'First Component',
+        component: FirstComponent,
+        children: [
+            {
+                path: 'child-a',
+                title: resolveChildATitle,
+                component: ChildAComponent,
+            },
+            {
+                path: 'child-b',
+                title: 'Child B',
+                component: ChildBComponent,
+            }
+        ],
+    },
+    {
+        path: 'second-component',
+        component: SecondComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'first-component',
+        pathMatch: 'full',
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    },
+];
+
